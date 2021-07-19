9c74241412cda87fad9a6189f94048c36e0ecde9
Kristen <kristenwood123@gmail.com>
2021-07-19 16:13:02 -0700

    added beer icon to main page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
+import { BiBeer } from 'react-icons/bi'
 
 
 const Details = () => {
    const { count } = useGlobalContext()
   return (
     <section className='details'>
-      <h1 className='header__heading'>Beer Fest 2021</h1>
-        <p className='count'>Attending: {count}</p>
-        <div className="text-container">
-          <h2>What:</h2>
-          <p style={{width: '75%', margin: '0 auto'}}>
-            Hey! It's that time of year for Beer Fest and I want to go with all of my closest friends! Which is why you're receiving this invite. The details are all below and I can't wait to spend the day with my favorite people :)</p> 
+      <div className="text-container">
+        <article className='details-what'>
+          <BiBeer className='beer-icon'/>
+            <h1 className='header__heading'>Beer Fest 2021</h1>
+            <h2>What:</h2>
+            <p style={{width: '75%', margin: '0 auto'}}>
+              Hey! It's that time of year for Beer Fest and I want to go with all of my closest friends! Which is why you're receiving this invite. The details are all below and I can't wait to spend the day with my favorite people :)</p> 
+        </article>
           <br></br><br></br>
           <p style={{marginBottom: '20px'}}>
             <b>Note: </b>Please check if you've been vaccinated! If not, please wear a mask!</p>
@@ -24,6 +27,7 @@ const Details = () => {
 
           <h2 className='details__header'>When:</h2>
             <p>12:00-5:00pm<br></br>Saturday October 1st, 2021</p>
+             <p className='count'>Attending: {count}</p>
         </div>
     </section>
   )
