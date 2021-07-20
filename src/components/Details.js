2cd013e0d40243e2da43a208517d750441566e51
Kristen <kristenwood123@gmail.com>
2021-07-20 08:29:16 -0700

    beer icon size smaller

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,25 +8,31 @@ const Details = () => {
   return (
     <section className='details'>
       <div className="text-container">
-        <article className='details-what'>
+        <article className='details-card'>
           <GiBeerStein className='beer-icon'/>
             <h1 className='header__heading'>Beer Fest 2021</h1>
             <p style={{width: '75%', margin: '0 auto'}}>
               Hey! It's that time of year for Beer Fest and I want to go with all of my closest friends! Which is why you're receiving this invite. The details are all below and I can't wait to spend the day with my favorite people :)</p> 
         </article>
-          <br></br><br></br>
-          <p style={{marginBottom: '20px'}}>
-            <b>Note: </b>Please check if you've been vaccinated! If not, please wear a mask!</p>
-          <hr className='hr'/>
-
+        <article className='details-card'>
           <h2 className='details__header'>Where:</h2>
           <p style={{marginBottom: '20px'}}>
             120 Madonna Rd, <br></br>San luis Obispo<br></br>CA, 92380</p>
-          <hr className='hr'/>
-
-          <h2 className='details__header'>When:</h2>
+            <h2 className='details__header'>When:</h2>
             <p>12:00-5:00pm<br></br>Saturday October 1st, 2021</p>
-             <p className='count'>Attending: {count}</p>
+        </article>
+         
+           <article className='details-card stats'>
+            <ul>
+              <li>{count}</li>
+              <li>
+                <p>total attending</p>
+              </li>
+              <li>56</li>
+              <li>days left</li>
+            </ul>
+            <button className='rsvp-btn'>RSVP</button>
+           </article>
         </div>
     </section>
   )
