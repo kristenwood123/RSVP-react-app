b5a70be271f13b6fba7f0e465ee0dae06d29c04e
Kristen <kristenwood123@gmail.com>
2021-08-03 14:33:12 -0700

    updated firebase initialization

--- src/firebase.js
+++ src/firebase.js
@@ -1,46 +1,15 @@
-var firebase = require('firebase') 
-var firebaseui = require('firebaseui')
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
-  let firebaseConfig = {
-    apiKey: "AIzaSyBUueKqJJCaRuMYZjQV_JeEk_2Too7j89c",
-    authDomain: "rsvp-react-app.firebaseapp.com",
-    projectId: "rsvp-react-app",
-    storageBucket: "rsvp-react-app.appspot.com",
-    messagingSenderId: "643867441344",
-    appId: "1:643867441344:web:17eddfac99a506cfa45525",
-    measurementId: "G-C24CZT0KGS"
-  };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
+  const app = firebase.initializeApp({
+     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT_ID
+  });
 
 
-  // Initialize the FirebaseUI Widget using Firebase.
-  const ui = new firebaseui.auth.AuthUI(firebase.auth());
