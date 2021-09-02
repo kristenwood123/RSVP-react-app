cc2777da06104ceda701f0c5999fa2609b745a7f
Kristen <kristenwood123@gmail.com>
2021-09-02 16:54:47 -0700

    centered rsvp form

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,43 @@
 import React from 'react'
-import { useGlobalContext } from '../contexts/context'
+
 
 const List = () => {
-  const { people } = useGlobalContext()
 
   return (
     <section className ='list'>
       <h1 className='list__header'>RSVP</h1>
-      <p>These people are coming, are you?</p>
+        <article className='attending'>
+          <form>
+            <div className='signup-section'>
+            <label htmlFor="name">First</label>
+            <input 
+              type="text" 
+              name='name'
+              required />
+          </div>  
+            <div className='signup-section'>
+            <label htmlFor="name">Last </label>
+            <input 
+              type="text" 
+              name='name'
+              required />
+          </div>    
 
-      <article className='attending'>
-        <form>
-        <fieldset>
-           <div className='signup-section'>
-          <label htmlFor="number">First</label>
-          <input 
-            type="text" 
-            name='name'
-            required />
-        </div>  
-          <div className='signup-section'>
-          <label htmlFor="number">Last </label>
-          <input 
-            type="text" 
-            name='name'
-            required />
-        </div>    
-          <h2>Can you attend?</h2>
-          {/* put in astericks */}
-          <label htmlFor="attending">Yes, I'll be there</label>
-           <input type="checkbox" name='attending'/>  
-          <label htmlFor="reject">No, I can't make it</label>
-           <input type="checkbox" name='reject'/>  
-           </fieldset>  
-      </form>
-      </article>
+            <h2>Can you attend?</h2>
+            <div className='rsvp'>
+            {/* put in astericks */}
+              <input type="checkbox" className='attending'/>
+            <label htmlFor="attending">
+              <span>Yes, I'll be there</span>
+            </label>
+            <input type="checkbox" className='reject'/>
+              <label htmlFor="attending">
+              <span>No, I can't make it</span>
+            </label>
+              </div>
+            <button type='submit' className='btn'>Submit</button>
+        </form>
+        </article>
     </section>
   )
 }
